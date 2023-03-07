14962068094c3c7b86b374cf55b99df53cdd1820
Akif Emre Senol <akifffemreee@gmail.com>
2023-03-07 18:15:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import User from "./components/User";
+
+const friends = [
+  {
+    id: 1,
+    name:"Ahmet",
+  },
+  {
+    id: 2,
+    name:"Mehmet",
+  },
+  {
+    id: 3,
+    name:"Ali",
+  }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <>
+    <User 
+    name="Akif Emre"
+    surname="Şenol"
+    isLoggedIn={true}
+    age={"23"}
+    friends={friends}
+    address={{
+      title:'İzmit/Kocaeli',
+      zip: 41654
+    }}
+    />
+    </>
+  )
 }
+
 
 export default App;
